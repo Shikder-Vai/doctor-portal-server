9f615235878d64e0097abf1a2f4deb164e1946d2
unknown <Shikder-Vai>
2022-06-04 01:04:03 +0600

    services collection added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const cli = require("nodemon/lib/cli");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,15 +11,37 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctorsprotal.lt3u5.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-app.get("/", (res, req) => {
-  res.send("hello doctors");
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.toArray();
+      res.send(service);
+    });
+  } finally {
+    //   await client.close()
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
 app.listen(port, () => {
   console.log(`doctors server runnig on ${port}`);
 });
